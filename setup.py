d15424a1c0f2b16a707a86a39bebbffab1ee44c6
Jacob <Jacobdenobel@gmail.com>
2020-11-20 10:14:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='modcma',
-    version="0.0.1",
+    version="0.0.2",
     author="Jacob de Nobel",
     author_email="jacobdenobel@gmail.com",
     description="Package Containing Modular CMA-ES optimizer",
