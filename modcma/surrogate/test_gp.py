88ba2e752d861de4e14399305a8dddb41447c456
tumpji@gmail.com <tumpji@gmail.com>
2023-01-06 22:20:03 +0100

    I'm too tired to know what I am doing

--- modcma/surrogate/test_gp.py
+++ modcma/surrogate/test_gp.py
@@ -1,3 +1,6 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 from abc import abstractmethod
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -6,61 +9,37 @@ import tensorflow_probability as tfp
 
 
 from .model import SurrogateModelBase
+from ..typing_utils import XType, YType
 
 tfb = tfp.bijectors
 tfd = tfp.distributions
 psd_kernels = tfp.math.psd_kernels
 
 
-class GP_ML_OptimizerBase(SurrogateModelBase):
-    ModelName = 'GP'
+class GP_ModelBase(SurrogateModelBase):
+    ModelName = 'Base_GP'
 
-    def __init__(self):
+    def __init__(self, parameters):
+        super().__init__(parameters)
         self.default_observation_noise_variance = tf.constant(np.exp(-5), dtype=tf.float64)
         self.observation_noise_variance = tfp.util.TransformedVariable(
             self.default_observation_noise_variance, tfb.Exp(), name='observation_noise_variance')
 
         self.kernel = self._kernel()
 
+    @abstractmethod
+    def _kernel(self):
+        pass
+
     @property
     def df(self):
         return 0
 
     @abstractmethod
-    def _kernel(self):
-        pass
-
-    @abstractmethod
-    def reset(self):
-        self.observation_noise_variance.assign(self.default_observation_noise_variance)
-
-    def _fit(self, X, Y):
-        self.reset()
+    def _fit(self, X: XType, F: YType, W: YType):
         self.observation_index_points = tf.constant(X, dtype=np.float64)
         self.observations = tf.constant(Y, dtype=np.float64)
-
-        gp = tfd.GaussianProcess(
-            kernel=self.kernel,
-            index_points=self.observation_index_points,
-            observation_noise_variance=self.observation_noise_variance)
-
-        optimizer = tf.optimizers.Adam(learning_rate=.05, beta_1=.5, beta_2=.99)
-
-        @tf.function
-        def optimize_gaussian_process():
-            with tf.GradientTape() as tape:
-                loss = -gp.log_prob(self.observations)
-            grads = tape.gradient(loss, gp.trainable_variables)
-            optimizer.apply_gradients(zip(grads, gp.trainable_variables))
-            return loss
-
-        for i in range(10000):
-            neg_log_likelihood = optimize_gaussian_process()
-            if i % 100 == 0:
-                print("Step {}: NLL = {}".format(i, neg_log_likelihood))
-        print('-'*80)
-        print(gp.trainable_variables)
-        print('-'*80)
+        return
 
     def _generate_regression_model(self, X) -> tfp.distributions.GaussianProcessRegressionModel:
         gprm = tfd.GaussianProcessRegressionModel(
@@ -81,10 +60,48 @@ class GP_ML_OptimizerBase(SurrogateModelBase):
         var = gprm.variance().numpy()
         return mean, var
 
-class GP_ML_ExponentiatedQuadratic(GP_ML_OptimizerBase):
+class GP_ML_ModelBase(GP_ModelBase):
+    ModelName = 'Base_GP_ML'
+
+    @abstractmethod
+    def reset(self):
+        self.observation_noise_variance.assign(self.default_observation_noise_variance)
+
+    def _fit(self, X, Y, W):
+        super()._fit(X, Y, W)
+        self.reset()
+
+        gp = tfd.GaussianProcess(
+            kernel=self.kernel,
+            index_points=self.observation_index_points,
+            observation_noise_variance=self.observation_noise_variance)
+
+        optimizer = tf.optimizers.Adam(learning_rate=.05, beta_1=.5, beta_2=.99)
+
+        #@tf.function
+        def optimize_gaussian_process():
+            with tf.GradientTape() as tape:
+                loss = -gp.log_prob(self.observations)
+                tf.print(loss)
+            import pdb; pdb.set_trace()
+            grads = tape.gradient(loss, gp.trainable_variables)
+            optimizer.apply_gradients(zip(grads, gp.trainable_variables))
+            return loss
+
+        for i in range(10000):
+            neg_log_likelihood = optimize_gaussian_process()
+            if i % 100 == 0:
+                print("Step {}: NLL = {}".format(i, neg_log_likelihood))
+        print('-'*80)
+        print(gp.trainable_variables)
+        print('-'*80)
+
+
+
+class GP_ML_ExponentiatedQuadratic(GP_ML_ModelBase):
     ModelName = 'ExponentiatedQuadratic'
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.default_amplitude = tf.constant(1., dtype=tf.float64)
         self.default_length_scale = self.default_amplitude
 
@@ -93,7 +110,7 @@ class GP_ML_ExponentiatedQuadratic(GP_ML_OptimizerBase):
         self.length_scale = tfp.util.TransformedVariable(
             self.default_length_scale, tfb.Exp(), name='length_scale')
 
-        super().__init__()
+        super().__init__(*args, **kwargs)
 
     def reset(self):
         self.amplitude.assign(self.default_amplitude)
@@ -104,16 +121,17 @@ class GP_ML_ExponentiatedQuadratic(GP_ML_OptimizerBase):
         return psd_kernels.ExponentiatedQuadratic(self.amplitude, self.length_scale)
 
 
+from ..parameters import Parameters
 
-
-gp = GP_ML_ExponentiatedQuadratic()
+p = Parameters(1)
+gp = GP_ML_ExponentiatedQuadratic(p)
 
 f = lambda x: np.sin(10*x[..., 0]) * np.exp(-x[..., 0]**2)
 X = np.random.uniform(-1., 1., 50)[..., np.newaxis]
 Y = f(X) + np.random.normal(0., .05, 50)
 
 
-gp._fit(X, Y)
+gp.fit(X, Y)
 
 index_points = np.linspace(-1., 1., 100)[..., np.newaxis]
 #samples = gp._predict(index_points).sample(10).numpy()
