99ed68c55682523b50e6781b2a109f0955551962
jacobdenobel <jacobdenobel@gmail.com>
2023-10-17 13:33:28 +0200

    change expected

--- tests/test_fmin.py
+++ tests/test_fmin.py
@@ -9,7 +9,7 @@ class TestFmin(unittest.TestCase):
 
     def test_best_so_far_storage(self):
         """Test storage of best so far individual."""
-        c = modularcmaes.ModularCMAES(sum, [1, 1,1,1,1])
+        c = modularcmaes.ModularCMAES(sum, 5)
         c.step()
         self.assertEqual(len(c.parameters.xopt), 5)
         self.assertEqual(sum(c.parameters.xopt), c.parameters.fopt)
@@ -20,3 +20,6 @@ class TestFmin(unittest.TestCase):
         self.assertEqual(sum(xopt), fopt)
         self.assertGreater(evaluations, 0)
         self.assertEqual(len(xopt), 5)
+
+
+    
