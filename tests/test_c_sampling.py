17fc82c21b601a3c0181582e5ada6ed7f1328b18
jacobdenobel <jacobdenobel@gmail.com>
2024-06-28 14:17:29 +0200

    added cached sampler

--- tests/test_c_sampling.py
+++ tests/test_c_sampling.py
@@ -19,19 +19,19 @@ class TestSampling(unittest.TestCase):
 
     def test_base_sampler_gauss(self):
         sampler = sampling.Gaussian(5)
-        self.sampler_test(sampler,  1.107994899)
+        self.sampler_test(sampler, 1.107994899)
 
     def test_base_sampler_halton(self):
-        sampler = sampling.Halton(5, 10)
-        self.sampler_test(sampler, 3.677252835)
+        sampler = sampling.Halton(5)
+        self.sampler_test(sampler, -0.3516482)
 
     def test_base_sampler_sobol(self):
-        sampler = sampling.Sobol(5, 10)
-        self.sampler_test(sampler, 1.787628108)
+        sampler = sampling.Sobol(5)
+        self.sampler_test(sampler, -0.5787853)
 
     def test_samplers_are_random(self):
         for sampler in (sampling.Halton, sampling.Sobol):
-            samples = np.array([sampler(5, 10)() for _ in range(5)])
+            samples = np.array([sampler(5)() for _ in range(5)])
             self.assertGreater(abs(samples[0] - samples).sum(), 1e-10)
 
     def test_mirrored(self):
@@ -40,9 +40,26 @@ class TestSampling(unittest.TestCase):
         sample2 = sampler()
         self.assertTrue(np.all(sample == -sample2))
 
-    # def test_cached_sampler(self):
-    #     sampelr = sampling.
+    def test_cached_sampler(self):
+        points = [[1, 1], [2, 2]]
+        sampler = sampling.CachedSampler(points)
+        self.assertEqual(sum(sampler()), 2)
+        self.assertEqual(sum(sampler()), 4)
+        self.assertEqual(sum(sampler()), 2)
 
+        sampler = sampling.CachedSampler(points, True)
+        self.assertEqual(sum(sampler()), float("inf"))
 
+        points = [[0.1, .1], [.2, .2]]
+        sampler = sampling.CachedSampler(points, True)
+        self.assertAlmostEqual(sum(sampler()), -2.5631031)
+        
+        
 if __name__ == "__main__":
     unittest.main()
+
+
+    '''
+        16, 17, 18, 23, 
+        
+    '''
