ae499dbac8aa84cb1d225f362e14eb855025e990
jacobdenobel <jacobdenobel@gmail.com>
2025-11-12 14:25:35 +0100

    remove dict union

--- tests/test_c_tuning.py
+++ tests/test_c_tuning.py
@@ -7,13 +7,13 @@ class TestTuning(unittest.TestCase):
     def test_module_options(self):
         options = c_maes.get_all_module_options()
         self.assertIsInstance(options, dict)
-        self.assertEqual(options, options | {"active": (False, True)})
-        self.assertEqual(options, options | {"elitist": (False, True)})
-        self.assertEqual(options, options | {"orthogonal": (False, True)})
-        self.assertEqual(options, options | {"sequential_selection": (False, True)})
-        self.assertEqual(options, options | {"threshold_convergence": (False, True)})
-        self.assertEqual(options, options | {"sample_sigma": (False, True)})
-        self.assertEqual(options, options | {"repelling_restart": (False, True)})
+        self.assertEqual(options, {**options, "active": (False, True)})
+        self.assertEqual(options, {**options, "elitist": (False, True)})
+        self.assertEqual(options, {**options, "orthogonal": (False, True)})
+        self.assertEqual(options, {**options, "sequential_selection": (False, True)})
+        self.assertEqual(options, {**options, "threshold_convergence": (False, True)})
+        self.assertEqual(options, {**options, "sample_sigma": (False, True)})
+        self.assertEqual(options, {**options, "repelling_restart": (False, True)})
 
 
     def test_configspace(self):
