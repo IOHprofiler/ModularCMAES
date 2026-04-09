df38dee08ab85c65d6ccaf5db5217208233d221d
jacobdenobel <jacobdenobel@gmail.com>
2026-04-09 10:43:45 +0200

    stuff

--- scripts/tuning/smac_info.py
+++ scripts/tuning/smac_info.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import json
+from pprint import pprint
 from time import sleep
 from argparse import ArgumentParser
 from collections import defaultdict
@@ -11,41 +12,37 @@ import numpy as np
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--fid", default=1, type=int)
-    parser.add_argument("--dim", default=2, type=int)
-    parser.add_argument("--root", default="smac", type=str)
+    parser.add_argument("--dim", default=5, type=int)
+    parser.add_argument("--root", default="data", type=str) 
     parser.add_argument("--show_all_feasible", action="store_true")
+    parser.add_argument("--all", action="store_true")
     args = parser.parse_args()
     
     folders = os.listdir(args.root)
-    arg_folder = f"BBOB_f{args.fid}_{args.dim}D_LRFalse"
+    arg_folder = f"BBOB_F{args.fid}_{args.dim}D_LRFalse"
+    
     for folder in sorted(os.listdir(args.root)):
         if not args.all and not folder.endswith(arg_folder): continue
-        filename = f"./{args.root}/{folder}/0/runhistory.json"
+        algf, *_ = os.listdir(os.path.join(args.root, folder))
+        filename = f"./{args.root}/{folder}/{algf}/0/runhistory.json"
         print(folder, end=' - ')
         for i in range(10):
             try:
                 with open(filename, "r") as f:
                     data = json.load(f)
                 break
-            except:
+            except Exception as err:
+                print(err)
                 sleep(.1)
         else:
             print("cannot load data")
             continue
         
-        for key, value in data['config_origins'].items():
-            if value == "Initial Design: Default configuration":
-                def_id = key 
-                break
-        else:
-            print("no default id")
-            continue
-
         configs = defaultdict(list)
         
         for config in data['data']:
             cost, cid = config['cost'], str(config['config_id'])
-            if cost != 1_000_000 or cid == def_id and np.isfinite(cost):
+            if cost != 1_000_000 and np.isfinite(cost):
                 configs[cid].append(float(cost))
 
         amin = float('inf')
@@ -58,14 +55,9 @@ if __name__ == "__main__":
                 amin = mvalue
                 cmin = cid
         print(f"{len(data['data'])} configs evaluated")
-        # if len(data['data']) != 10_000:
-        print(f"default solution ({def_id}):",
-            np.mean(configs[def_id]), 
-            data['configs'][def_id], 
-            len(configs[def_id])
-        )
         if cmin is None:
             print("No best solutions yet")
         else:
-            print(f"lowest avg. cost ({cmin}):", amin, data['configs'][cmin])     
+            print(f"lowest avg. cost ({cmin}):", amin)
+            pprint(data['configs'][cmin])
         print()        
